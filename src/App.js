4e93aa7ede437efdaa38938d440785c76c86a904
Opeyemi Peter <opeyemiadebowale1759@gmail.com>
2021-03-11 20:52:38 -0800

    set up home page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import "./App.css";
 import Header from "./compos/Header";
+import Home from "./compos/Home";
+
 
 function App() {
   return (
@@ -7,6 +9,7 @@ function App() {
       {/* header */}
       <Header />
       {/* body */}
+      <Home/>
     </div>
   );
 }
