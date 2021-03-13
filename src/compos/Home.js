2aa469a72eee7a567dff09706669fda78a8c0ec1
Opeyemi Peter <opeyemiadebowale1759@gmail.com>
2021-03-13 00:10:08 -0800

    home page imgs

--- src/compos/Home.js
+++ src/compos/Home.js
@@ -1,27 +1,25 @@
 import React from "react";
 import "./Home.css";
 import Product from "./Product";
+
+import homePage from "./images/homepage.jpg";
+
 function Home() {
   return (
     <div className="home">
       <div className="home__container">
-        <img
-          src="https://images-eu.ssl-images-amazon.com/images/G/02/digital/video/merch2016/Hero/Covid19/Generic/GWBleedingHero_ENG_COVIDUPDATE__XSite_1500x600_PV_en-GB._CB428684220_.jpg"
-          alt="home-img"
-          className="home__image"
-        />
+        <img src={homePage} alt="home-img" className="home__image" />
         <div className="home__row">
-          <Product/>
-          {/* product */}
-          {/* product */}
+          <Product />
+          <Product />
         </div>
         <div className="home__row">
-          {/* product */}
-          {/* product */}
-          {/* product */}
+          <Product />
+          <Product />
+          <Product />
         </div>
         <div className="home__row">
-          {/* product */}
+          <Product />
         </div>
       </div>
     </div>
