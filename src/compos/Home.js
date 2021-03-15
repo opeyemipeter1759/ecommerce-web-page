2bc21fca3660848f38286003ef74dc61fe40b5e1
Opeyemi Peter <opeyemiadebowale1759@gmail.com>
2021-03-14 23:06:50 -0700

    home page product images

--- src/compos/Home.js
+++ src/compos/Home.js
@@ -4,6 +4,11 @@ import Product from "./Product";
 import homePage from "./images/homepage.jpg";
 
 import lean from "./images/lean-startup.jpg";
+import mixer from "./images/mixer.jpg";
+import smartwatch from "./images/smartwatch.jpg";
+import speaker from "./images/speaker.jpg";
+import ipad from "./images/ipad.jpg";
+import monitor from "./images/monitor.jpg";
 
 function Home() {
   return (
@@ -12,20 +17,51 @@ function Home() {
         <img src={homePage} alt="home-img" className="home__image" />
         <div className="home__row">
           <Product
+            id="001"
             title="The Lean Startup"
             price={29.99}
             image={lean}
             rating={5}
           />
-          <Product />
+          <Product
+            id="002"
+            title="Kenwood kMix Stand Mixer for Baking, Stylish Kitchen Mixer with K-beater, Dough Hook and Whisk, 5 Litre Glass Bowl "
+            price={239.0}
+            rating={4}
+            image={mixer}
+          />
         </div>
         <div className="home__row">
-          <Product />
-          <Product />
-          <Product />
+          <Product
+            id="003"
+            title="Samsung LC49RG90SSUXEN 49'Curved LED Gaming Monitor -- Super Ultra Wide Dual WQHD 5120 x 1400"
+            price={199.99}
+            rating={3}
+            image={smartwatch}
+          />
+          <Product
+            id="004"
+            title="Amazon Echo (3rd generation) | Smart speaker with Alexa, Charcoal Fabric"
+            price={98.99}
+            rating={5}
+            image={speaker}
+          />
+          <Product
+            id="005"
+            title="Apple iPad Pro (12.9-inch, Wi-Fi, 128GB) - Space Gray (4th Generation) (2020)"
+            price={548}
+            rating={4}
+            image={ipad}
+          />
         </div>
         <div className="home__row">
-          <Product />
+          <Product
+            id="006"
+            title="Samsung LC49RG90SSUXEN 49 Curved LED Gaming Monitor -- Super Ultra Wide Dual WQHD 5120 x 1400"
+            price={539}
+            rating={5}
+            image={monitor}
+          />
         </div>
       </div>
     </div>
--- src/compos/Product.js
+++ src/compos/Product.js
@@ -1,12 +1,11 @@
 import React from "react";
 import "./product.css";
-import lean from "./images/lean-startup.jpg";
 
 function Product({ id, title, image, rating, price }) {
   return (
     <div className="product">
       <div className="product__info">
-        <p>{title}</p>
+        <p className="product__title">{title}</p>
         <p className="product__price">
           <small>$</small>
           <strong>{price}</strong>
