079881506259977593fc411598be9d13318f189b
Opeyemi Peter <opeyemiadebowale1759@gmail.com>
2021-03-16 00:08:36 -0700

    add to cart

--- src/compos/Checkout.js
+++ src/compos/Checkout.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./Checkout.css";
 import checkout_ad from "./images/cheackout_ad.jpg";
+import Subtotal from "./Subtotal";
 
 function Checkout() {
   return (
@@ -9,11 +10,11 @@ function Checkout() {
         <img src={checkout_ad} alt="" className="checkout__ad" />
         <div className="">
           <h2 className="checkout__title">Shopping Cart</h2>
-          
+
         </div>
       </div>
       <div className="checkout__right">
-        <h2>the subtotal</h2>
+        <Subtotal/>
       </div>
     </div>
   );
