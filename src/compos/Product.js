0e5e9e686210269ed8dd260e735892cc12712994
Opeyemi Peter <opeyemiadebowale1759@gmail.com>
2021-03-18 00:43:58 -0700

    contect api

--- src/compos/Product.js
+++ src/compos/Product.js
@@ -1,7 +1,21 @@
 import React from "react";
 import "./product.css";
+import { useStateValue } from "../ServiceProvider";
 
 function Product({ id, title, image, rating, price }) {
+  const [{ basket }, dispatch] = useStateValue();
+  const addToBasket = () => {
+    dispatch({
+      type: "ADD_TO_BASKET",
+      item: {
+        id: id,
+        title: title,
+        image: image,
+        price: price,
+        rating: rating,
+      },
+    });
+  };
   return (
     <div className="product">
       <div className="product__info">
@@ -19,7 +33,7 @@ function Product({ id, title, image, rating, price }) {
         </div>
       </div>
       <img src={image} alt="" srcset="" />
-      <button>Add to Cart</button>
+      <button onClick={addToBasket}>Add to Cart</button>
     </div>
   );
 }
--- src/reducer.js
+++ src/reducer.js
@@ -3,9 +3,10 @@ export const initialState = {
 };
 
 const reducer = (state, action) => {
+  console.log(action)
   switch (action.type) {
     case "ADD_TO_BASKET":
-      return { ...state, basket: [...state.basket, action.item] };
+      return { ...state, basket: [...state.basket, action.item],};
     default:
       return state;
   }
