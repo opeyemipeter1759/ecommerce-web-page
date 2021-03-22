fe53081448ac01969c8a8a89aa58eaf6f92d6f06
Opeyemi Peter <opeyemiadebowale1759@gmail.com>
2021-03-22 12:46:44 -0700

    email auth

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
+import React, { useEffect } from "react";
 import "./App.css";
 import Header from "./compos/Header";
 import Home from "./compos/Home";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import Checkout from "./compos/Checkout";
 import Login from "./compos/Login";
+import { auth } from "./firebase";
+import { useStateValue } from "./ServiceProvider";
 
-function App() {
+function App()
+{
+  const =[{ }, dispatch] = useStateValue();
+  useEffect(() => {
+    auth.onAuthStateChanged((authUser) => {
+      console.log( "The user is >>> ", authUser );
+      if (authUser) {
+        dispatch( {
+          type: 'SET_USER',
+          user: authUser,
+        })
+      } else
+      {
+        dispatch( {
+          type: 'SET_USER',
+          user: null,
+        })
+      }
+    });
+  }, []);
   return (
     <BrowserRouter>
       <div className="app">
--- src/reducer.js
+++ src/reducer.js
@@ -1,5 +1,6 @@
 export const initialState = {
   basket: [],
+  user: null,
 };
 export const getBasketTotal = (basket) =>
   basket?.reduce((amount, item) => item.price + amount, 0);
@@ -23,6 +24,11 @@ const reducer = (state, action) => {
       return { ...state, basket: newBasket };
     default:
       return state;
+    case "SET_USER":
+      return {
+        ...state,
+        user: action.user
+      }
   }
 };
 
